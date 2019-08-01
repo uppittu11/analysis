8ac4dfefe760de7ec9f767a7c2dcd0a51fa1e6dd
Parashara Shamaprasad <p.shama@vanderbilt.edu>
2019-08-01 15:18:49 -0500

    added script to print data from a pickle file to the helpers folder

--- analyze-aa/helpers/print_data.py
+++ analyze-aa/helpers/print_data.py
@@ -6,16 +6,14 @@ import pickle
 with open('results.p', 'rb') as f:
     results = pickle.load(f)
 
-print(results)
-
 # Check if the results are for a bilayer (top and bottom) or
 # for a multilayer (no distinction)
 if len(results[0]) <= 5:
     tilt = results[:,:2]
-    print("Tilt :   {:.5} +/- {:.5}".format(np.mean(tilt[:,0]), np.linalg.norm(tilt[:,1])))
+    print("Tilt :   {:.5f} +/- {:.5f}".format(np.mean(tilt[:,0]), np.linalg.norm(tilt[:,1])))
     s2 = results[:,2]
-    print("S2 :     {:.5} +/- {:.5}".format(np.mean(s2), stats.sem(s2)))
-    apl = results[:,3]
-    print("APL :    {:.5} +/- {:.5}".format(np.mean(apl), stats.sem(apl)))
-    height = results[:,4]
-    print("Height : {:.5} +/- {:.5}".format(np.mean(height), stats.sem(height)))
+    print("S2 :     {:.5f} +/- {:.5f}".format(np.mean(s2), stats.sem(s2)))
+    apl = results[:,3] * 36
+    print("APL :    {:.5f} +/- {:.5f}".format(np.mean(apl), stats.sem(apl)))
+    height = results[:,4] * 6
+    print("Height : {:.5f} +/- {:.5f}".format(np.mean(height), stats.sem(height)))
