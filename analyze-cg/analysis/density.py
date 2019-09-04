Fixed new height calculation in analyze.py

analyze-cg/analysis/density.py
@@ -4,21 +4,22 @@ from .molecules import molecule
 from .smoothing import savitzky_golay
 from scipy.signal import find_peaks
 
-def calc_peaks(frame, atomselection, n_frames, window, n_layers, masses):
+def calc_peaks(frame, atomselection, n_layers, masses, window=40):
     atoms = frame.top.select(atomselection) # which atoms to plot
     box_length = np.mean([frame.unitcell_lengths[0,2]])
-    hist, edges = np.histogram(frame.xyz[0, atoms, 2].reshape(-1), weights=np.tile(masses.take(atoms), n_frames),
-                               range=[-.01,box_length+.01], bins=400)
+    hist, edges = np.histogram(frame.xyz[0, atoms, 2].reshape(-1), 
+                                weights=masses.take(atoms),
+                                range=[-.01,box_length+.01], bins=400)
     bins = (edges[1:]+edges[:-1]) / 2.0
-    hist = savitzky_golay(hist, 40, 5)
+    hist = savitzky_golay(hist, window, 5)
     peaks, _ = find_peaks(hist, prominence=np.max(hist)*0.25)
     assert len(peaks) == n_layers
     peaks = bins[peaks]
 
     return peaks
 
-def calc_height(frame, atomselection, n_frames, window, n_layers, masses):
-    peaks = calc_peaks(frame, atomselection, n_frames, window, n_layers, masses)
+def calc_height(frame, atomselection, n_layers, masses,  window=40):
+    peaks = calc_peaks(frame, atomselection, window, n_layers, masses)
     peaks = np.sort(peaks)
     height = peaks[1:] - peaks[:-1]
     return height

analyze-cg/analyze.py
@@ -6,7 +6,7 @@ from xml.etree import cElementTree as ET
 import numpy as np
 import scipy.stats as stats
 import mdtraj as md
-import analysis.utils
+import analysis
 from analysis.molecules import molecule
 
 ## TO USE THIS SCRIPT ##
@@ -40,7 +40,7 @@ def analyze_all(frame, masses, n_leaflets):
 
     # Calculate the height -- uses the "head" atoms specified below
     atomselection = 'name mhead2 oh1 oh2 oh3 oh4 oh5 amide chead head'
-    height = analysis.utils.calc_height(frame, atomselection,int(n_leaflets/2+1), masses)
+    height = analysis.density.calc_height(frame, atomselection, int(n_leaflets/2+1), masses)
     return [np.array(tilt), np.mean(s2), apl, np.array(height)]
 
 def main():