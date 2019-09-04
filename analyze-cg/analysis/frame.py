8ac8e6587fdf046c8075ca3e6ee3a511301ce7a5
Parashara Shamaprasad <p.shama@vanderbilt.edu>
2019-09-04 16:44:37 -0500

    Added atomnames attribute to frame

--- analyze-cg/analysis/frame.py
+++ analyze-cg/analysis/frame.py
@@ -22,56 +22,65 @@ class Frame(object):
 
     """
 
-    def __init__(self, xyz=[], unitcell_lengths=[], masses=[], residuelist=[], n_leaflets=2):
-        self.xyz = xyz
-        self.unitcell_lengths = unitcell_lengths
-        self.masses = masses
-        self.residuelist = residuelist
+    def __init__(self, xyz=[], unitcell_lengths=[], masses=[], residuelist=[], atomnames=[], n_leaflets=2):
+        self._xyz = xyz
+        self._unitcell_lengths = unitcell_lengths
+        self._masses = masses
+        self._residuelist = residuelist
+        self._n_leaflets = n_leaflets
+        self._atomnames = atomnames
 
     @property
     def xyz(self):
-        return self.xyz
+        return self._xyz
     
     @xyz.setter
     def xyz(self, xyz):
-        assert type(xyz) == np.ndarray
-        self.xyz = xyz
+        self._xyz = xyz
     
     @property
     def unitcell_lengths(self):
-        return self.unitcell_lengths
+        return self._unitcell_lengths
 
     @unitcell_lengths.setter
     def unitcell_lengths(self, unitcell_lengths):
-        assert type(unitcell_lengths) == np.ndarray
-        self.unitcell_lengths = unitcell_lengths
+        self._unitcell_lengths = unitcell_lengths
 
     @property
     def masses(self):
-        return self.masses
+        return self._masses
     
     @masses.setter
     def masses(self, masses):
-        assert type(masses) == np.ndarray
-        self.masses = masses
+        self._masses = masses
 
     @property
     def residuelist(self):
-        return self.residuelist
+        return self._residuelist
     
     @residuelist.setter
     def residuelist(self, residuelist):
-        assert type(residuelist) == np.ndarray
-        self.residuelist = residuelist
+        self._residuelist = residuelist
+    
+    @property
+    def atomnames(self):
+        return self._atomnames
+    
+    @atomnames.setter
+    def atomnames(self, atomnames):
+        self._atomnames = atomnames
 
     @property
     def n_leaflets(self):
-        return self.n_leaflets
+        return self._n_leaflets
     
     @n_leaflets.setter
     def n_leaflets(self, n_leaflets):
         n_leaflets == int(n_leaflets)
-        self.n_leaflets = n_leaflets
+        self._n_leaflets = n_leaflets
+    
+    def __repr__(self):
+        return "Frame with {} residues and {} atoms".format(len(self._residuelist), len(self.masses))
     
     def validate_frame(self):
         """ Ensure that this frame can be analyzed.
@@ -84,11 +93,12 @@ class Frame(object):
         Raises an assertion error in the case that there is a mismatch in
         dimensions/types
         """
-        self.masses = np.array(self.masses)
-        self.unitcell_lengths = np.array(self.unitcell_lengths)
-        self.xyz = np.array(self.xyz)
+        self._masses = np.array(self._masses)
+        self._unitcell_lengths = np.array(self._unitcell_lengths)
+        self._xyz = np.array(self._xyz)
+        self._atomnames = np.array(self.atomnames)
 
-        assert self.masses.shape[0] == self.xyz.shape[0]
-        assert self.xyz.shape[0] == len(self.residuelist)
-        assert self.unitcell_lengths.shape[0] == 3
-        assert type(self.n_leaflets) == int
+        assert self._masses.shape[0] == self._xyz.shape[0]
+        assert self._masses.shape[0] == self._atomnames.shape[0]
+        assert self._unitcell_lengths.shape[0] == 3
+        assert type(self._n_leaflets) == int
