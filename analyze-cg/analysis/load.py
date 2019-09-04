65cf0701c34e35aa3c487c833b89194e6548deae
Parashara Shamaprasad <p.shama@vanderbilt.edu>
2019-09-04 10:47:12 -0500

    Added a function to create a residue class from a md.Topology

--- analyze-cg/analysis/load.py
+++ analyze-cg/analysis/load.py
@@ -1,18 +1,38 @@
 import mdtraj as md
-from .molecules import *
+import numpy as np
+from .molecules import molecule
+from .residue import Residue
 
-def load_system(traj, system):
-    start = 0
-    for mol in system:
-        for index in range(mol[1]):
-            traj.topology.residue(index+start).name = mol[0]
-        start += mol[1]
-    return traj
+def _is_lipid(resname):
+    """ Determine whether a residue name is an analyzeable lipid
 
-def is_lipid(resname):
+    Parameters:
+    -----------
+    resname : string
+        name of the residue to check
+
+    Returns:
+    --------
+    boolean
+        True if resnam is in molecule. False otherwise
+    """
     return (resname in molecule)
 
 def get_standard_topology(traj):
+    """ Load system information into a trajectory.
+    Based on the number and name of CG beads in a residue, load in the
+    correct residue name
+
+    Parameters:
+    -----------
+    traj : md.Trajectory
+        Trajectory in which system information is loaded.
+
+    Returns:
+    --------
+    traj : md.Trajectory
+        Trajectory with information loaded.
+    """
     for i, residue in enumerate(traj.top.residues):
         atoms = set([atom.name for atom in residue.atoms])
         if 'chead' in atoms:
@@ -39,3 +59,31 @@ def get_standard_topology(traj):
         traj.top.residue(i).name = name
 
     return traj
+
+def to_residuelist(topology):
+    """ Convert a topology into a list of Residue objects.
+    Residue objects are intended to reduce memory consumption by
+    eliminating unnecessary information.
+
+    Parameters:
+    -----------
+    topology : md.Topology
+        Topology to be convert
+
+    Returns:
+    --------
+    residuelist : list
+        list of Residue objects
+    """
+    assert type(topology) == md.Topology
+    residuelist = []
+    for residue in topology:
+        if not _is_lipid(residue.name):
+            continue
+        res_idx = topology.select('residue {}'.format(residue.index))
+        tails = []
+        for tail_idx in molecule[residue.name][1]:
+            tails.append(np.array(res_idx).take(tail_idx))
+        new_residue = Residue(name=residue.name, tails=tails)
+        residuelist.append(new_residue)
+    return residuelist
