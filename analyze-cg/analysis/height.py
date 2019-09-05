acee355dce30fa057f7ffb7be78c2de7e6b56bb8
Parashara Shamaprasad <p.shama@vanderbilt.edu>
2019-09-05 09:09:53 -0500

    Removed extraneous calculations in height.py

--- analyze-cg/analysis/height.py
+++ analyze-cg/analysis/height.py
@@ -4,22 +4,35 @@ from .molecules import molecule
 from .smoothing import savitzky_golay
 from scipy.signal import find_peaks
 
-def calc_peaks(frame, atomselection, n_layers, masses, window=40):
-    atoms = frame.top.select(atomselection) # which atoms to plot
-    box_length = np.mean([frame.unitcell_lengths[0,2]])
-    hist, edges = np.histogram(frame.xyz[0, atoms, 2].reshape(-1), 
-                                weights=masses.take(atoms),
-                                range=[-.01,box_length+.01], bins=400)
-    bins = (edges[1:]+edges[:-1]) / 2.0
+def calc_peaks(frame, atomselection, window=41):
+    n_layers = int(frame.n_leaflets/2+1)
+    atoms = frame.select(atomselection) # which atoms to plot
+    box_length = frame.unitcell_lengths[2]
+
+    z = frame.xyz[atoms, 2].reshape(-1) - np.mean(frame.xyz[atoms, 2])
+    z_range = [-box_length*0.5-.01,box_length*0.5+.01]
+
+    hist, edges = np.histogram(z, weights=frame.masses.take(atoms),
+                                range=z_range, bins=400)
+    bins = (edges[1:]+edges[:-1]) * 0.5
     hist = savitzky_golay(hist, window, 5)
     peaks, _ = find_peaks(hist, prominence=np.max(hist)*0.25)
-    assert len(peaks) == n_layers
+    peaks = np.sort(peaks)
     peaks = bins[peaks]
+
+    if len(peaks) != n_layers:
+        print("There is an unequal number of peaks ({}) and layers ({})".format(len(peaks), n_layers))
+        if len(peaks) > n_layers:
+            peaks = peaks[:n_layers]
+        else:
+            for _ in range(n_layers-len(peaks)):
+                peaks.append(2*peaks[-1]-peaks[-2])
+    
 
     return peaks
 
-def calc_height(frame, atomselection, n_layers, masses,  window=40):
-    peaks = calc_peaks(frame, atomselection, window, n_layers, masses)
+def calc_height(frame, atomselection, window=41):
+    peaks = calc_peaks(frame, atomselection, window)
     peaks = np.sort(peaks)
     height = peaks[1:] - peaks[:-1]
     return height
