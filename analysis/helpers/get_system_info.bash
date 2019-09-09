438972788892d2c2ac95b91b3999b6d56f41d1ab
Parashara Shamaprasad <p.shama@vanderbilt.edu>
2019-09-09 11:09:48 -0500

    Removed irrelevant helper scripts

--- analysis/helpers/get_system_info.bash
+++
@@ -1,6 +0,0 @@
-for dir in ../../mixed* ../../pure*; do
-    echo $dir
-    mv $dir/system.info $dir/aa_system.info
-    python get_system_info.py $dir/branch1/5_npt/cg/start.hoomdxml $dir/cg_system.info
-done
-
