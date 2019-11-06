7feaf6669922d905ebb916e6f60fb6772502f73e
Parashara Shamaprasad <p.shama@vanderbilt.edu>
2019-11-06 00:08:55 -0600

    Add tests for all molecule functionality

--- analysis/tests/test_molecules.py
+++ analysis/tests/test_molecules.py
@@ -15,12 +15,39 @@ class TestMolecules(ConfTest):
         mols = collect_molecules("atomistic")
         assert len(mols) > 0
 
-    def test_molecule_validator(self):
+    def test_name(self):
         from analysis.molecules.molecules import Molecule
         molecule = Molecule()
         molecule.name = "Ceramide"
-        molecule.add_tail([1, 2, 3])
-        molecule.head = 0
+        assert molecule.name == "Ceramide"
         with pytest.raises(TypeError):
             molecule.name = [1, 2, 3]
+
+    def test_head(self):
+        from analysis.molecules.molecules import Molecule
+        molecule = Molecule()
+        molecule.head = 5
+        assert molecule.head == 5
+        with pytest.raises(TypeError):
+            molecule.head = 2.5
+
+    def test_n_atoms(self):
+        from analysis.molecules.molecules import Molecule
+        molecule = Molecule()
+        molecule.n_atoms = 3
+        assert molecule.n_atoms == 3
+        with pytest.raises(TypeError):
+            molecule.n_atoms = "three"
+
+    def test_tails(self):
+        from analysis.molecules.molecules import Molecule
+        molecule = Molecule()
+        molecule.add_tail([1, 2, 3])
+        molecule.add_tail((4, 5, 6))
+        molecule.add_tail([1, 2, 3])
+        molecule.remove_tail([1, 2, 3])
+        molecule.remove_tail(1)
+        assert molecule.tails == [(4, 5, 6)]
+        with pytest.raises(TypeError):
+            molecule.add_tail(5)
 
